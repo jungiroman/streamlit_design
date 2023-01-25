517839f389e08052cfee9ae738294a012f130eec
roman.jungi <roman.jungi@gmail.com>
2023-01-25 13:25:38 +0100

    Initial commit

---
+++ app.py
@@ -0,0 +1,62 @@
+import streamlit as st
+import mysql.connector
+import pandas as pd
+
+
+@st.experimental_memo(ttl=3600)
+def read_from_db():
+    db = mysql.connector.connect(**st.secrets["design"])
+    cursor = db.cursor()
+    table = 'articles'
+    columns = "*"
+    query = "SELECT " + columns + " FROM " + table #+ condition
+    cursor.execute(query)
+    f = cursor.fetchall()
+    df = pd.DataFrame(f)
+    cursor.execute("SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = '" + table + "' AND TABLE_SCHEMA = 'u831348299_rj_design'")
+    xs = cursor.fetchall()
+    df.columns = (x[0] for x in xs)
+    return df
+
+
+if __name__ == "__main__":
+    st.set_page_config(page_title="Design", page_icon=":newspaper:", layout="wide")
+    st.title('designboom')
+    data = read_from_db()
+    data = data.sort_values(by=['published'], ascending=False)
+    data.reset_index(inplace=True, drop=True)
+
+    for ind, article in data.iterrows():
+        url = article.link
+        title = article.title
+
+        first_image = article.first_image
+        images = article.images.split(';')
+
+        col1, col2 = st.columns(2)
+        with col1:
+            st.write("### " + title)
+            published = article.published
+            authors = article.authors
+            st.write(str(published) + "  /  " + authors)
+            summary = article.summary
+            st.markdown(summary, unsafe_allow_html=True)
+            if st.button('Load images', key="btn"+str(ind)):
+                for img in images:
+                    st.image(img)
+
+        with col2:
+            st.markdown(f'''
+                                <a href="{url}">
+                                    <img src="{first_image}" style="width:100%" alt="{url}" />
+                                </a>''',
+                        unsafe_allow_html=True
+                        )
+        st.write('---')
+
+
+        #st.write(article[7])
+
+
+
+
---
+++ main.py
@@ -0,0 +1,92 @@
+from dotenv import load_dotenv
+import os
+import feedparser
+from lxml import html
+import requests
+import pandas as pd
+from bs4 import BeautifulSoup
+from textblob import TextBlob
+import nltk
+from gensim.parsing.preprocessing import remove_stopwords
+import re
+import mysql.connector
+from datetime import datetime
+nltk.download('wordnet')
+
+
+def get_feed(url):
+    feed = requests.get(url).content
+    feed = feedparser.parse(feed)
+    return feed
+
+
+def process_text(text):
+    text = text.lower()
+    text = re.sub(r'[.,"\'-?:!;’‘]', '', text)
+    #text = text.replace(''', '')
+    text = remove_stopwords(text)
+    text = " ".join([word.lemmatize() for word in TextBlob(text).words])
+    return text
+
+
+def get_images(url):
+    response = requests.get(url)
+    root = html.fromstring(response.content)
+    image_url = root.xpath("//img/@src")[0]
+    image_urls = root.xpath("//div[@class='page-content']//img/@data-src")
+    return image_url, image_urls
+
+
+def store_in_db(record):
+    db = mysql.connector.connect(
+        host=os.environ.get('host'),
+        database=os.environ.get('database'),
+        user=os.environ.get('user'),
+        password=os.environ.get('password')
+    )
+
+    cursor = db.cursor()
+    cursor.execute("CREATE TABLE IF NOT EXISTS articles (link VARCHAR(255) PRIMARY KEY, title VARCHAR(1024), published TIMESTAMP, authors VARCHAR(1024), first_image VARCHAR(1024), images VARCHAR(65535), summary VARCHAR(1024), search_text VARCHAR(2048))")
+    sql = "INSERT IGNORE INTO articles (link, title, published, authors, first_image, images, summary, search_text) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
+    values = (record['link'], record['title'], record['published'], record['authors'], record['first_image'], record['images'], record['summary'], record['search_text'])
+    cursor.execute(sql, values)
+    db.commit()
+
+
+def process(articles):
+    for article in articles:
+        link = article['link']
+        title = article['title']
+        published = article['published']
+        published = datetime.strptime(published, "%a, %d %b %Y %H:%M:%S %z").strftime("%Y-%m-%d %H:%M:%S")
+        authors = (x['name'] for x in article['authors'])
+        authors = ";".join(authors)
+        first_image, images = get_images(link)
+        images = ";".join(images)
+        summary = article.summary.replace('<img src="https://www.designboom.com/feed/" /><br />', '')
+        summary = summary.replace('<img src="" /><br />', '')
+        soup = BeautifulSoup(summary, 'html.parser')
+        text = soup.p.get_text()
+        text = title + " " + text
+        text = process_text(text)
+        record = {
+            'link': link,
+            'title': title,
+            'published': published,
+            'authors': authors,
+            'first_image': str(first_image),
+            'images': images,
+            'summary': summary,
+            'search_text': text
+        }
+        store_in_db(record)
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    load_dotenv()
+    #db_user = os.environ.get('secretUser')
+    #db_pw = os.environ.get('secretKey')
+    url = 'https://www.designboom.com/feed/'
+    feed = get_feed(url)
+    process(feed.entries)
