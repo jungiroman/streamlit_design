1e4f5dbc9eea20d637025b3e0e3d134058e7f27b
roman.jungi <roman.jungi@gmail.com>
2023-01-26 18:19:05 +0100

    developing

--- app.py
+++ app.py
@@ -23,8 +23,18 @@ def process_text(text):
     return text
 
 
+def hide_header():
+    hide_decoration_bar_style = '''
+                <style>
+                    header {visibility: hidden;}
+                </style>
+            '''
+    st.markdown(hide_decoration_bar_style, unsafe_allow_html=True)
+
+
 if __name__ == "__main__":
-    st.set_page_config(page_title="design feed", page_icon="random", layout="wide")
+    st.set_page_config(page_title="design feed", page_icon=":random:", layout="wide")
+    hide_header()
     st.title('design feed')
     if 'page' not in st.session_state:
         st.session_state['page'] = 0
